b05ef556e62a235945535c756d4611a561f213ed
hippye99 <hipp.ye@presslogic.com>
2026-03-25 17:56:16 +0800

    feat: .history目录放入黑名单

--- .history/components/ui/LoginPop_20260311172806.tsx
+++
@@ -1,6 +0,0 @@
-import Image from "next/image";
-import { useState } from "react";
-// LoginPop.tsx
-export const LoginPop = () => {
-  // ...组件内容
-};
--- .history/components/ui/LoginPop_20260311174508.tsx
+++
@@ -1,3 +0,0 @@
-import Image from "next/image";
-import { useState } from "react";
-// LoginPop.tsx
